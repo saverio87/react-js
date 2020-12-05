40475796b5a0abd37c9f9df167699aa48dae93a9
sav87 <mattolisaverio@gmail.com>
2020-12-05 19:38:49 +0800

    Update database config

---
+++ config/env/production/database.js
@@ -0,0 +1,23 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({env}) => ({
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "postgres",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
+        "ssl": { "rejectUnauthorized": false }
+      },
+      "options": {
+        ssl: false,
+      }
+    }
+  }
+});
